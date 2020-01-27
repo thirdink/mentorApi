6f7de0277510d160436501caa3466cc3d5f3d2a6
Rumit Tuladhar <rumitwashere@gmail.com>
2020-01-27 15:51:23 +1030

    user sign up

---
+++ src/config/constants.js
@@ -0,0 +1,22 @@
+const devConfig =   { MONGO_URL: 'mongodb://localhost/mentorjsapi-dev' };
+const testConfig =  { MONGO_URL: 'mongodb://localhost/mentorjsapi-test'};
+const prodConfig =  { MONGO_URL: 'mongodb://localhost/mentorjsapi-prod'};
+const defaultConfig = {
+        PORT: process.env.PORT || 3000,
+    };
+    function envConfig(env) {
+        switch (env) {
+            case 'development':
+                return devConfig;
+            case 'test':
+                return testConfig;
+            default:
+                return prodConfig;
+        }
+    }
+    
+    //Take defaultConfig and make it a single object 
+    //So, we have concatenated two objects into one 
+    export default { ...defaultConfig,
+        ...envConfig(process.env.NODE_ENV),
+    };
